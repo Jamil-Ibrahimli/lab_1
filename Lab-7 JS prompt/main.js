869c6ffc28a5105681ac6f1c27e3d8b29a94b1df
Jamil-Ibrahimli~ <ibrahimlicemil@example.com>
2023-10-29 22:07:35 +0400

    first js lab

---
+++ Lab-7 JS prompt/main.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+// let country = prompt("Enter a Country");
+
+// switch(country){
+// case "Azerbaijan": alert(' The capital is Baku');
+// break;
+
+// case "Russia" : alert('The capital is Moscow');
+// break;
+// case "Turkiye": alert(' The Captal is Ankara');
+// break;
+
+// default: alert ("This country does not exist")
+
+
+// }
+
+
+
+
+
+
+
+
+
+// let sec = parseInt(prompt("Enter yur secund"));
+
+// let min = Math.floor(sec/ 60);
+
+// let extraSec = sec%60;
+
+
+// alert(min +' '+"minute"+' '+extraSec+' '+"secund");
+
+
+
+
+
+
+
+
+
+// let val = parseInt(prompt("Enter a Value"));
+
+// let fact = 1 
+
+
+// if(val===0 ){alert( "Factorial of 0 = 1");}
+
+// else if(val<0){alert(" Not exist!!!!!")}
+
+// else{
+// for(let i = 1;i <= val ; i++){
+
+//     fact = fact * i
+// }
+
+// alert("Factorial is "+' '+fact);}
+
+
+
+
+
+
+
+
