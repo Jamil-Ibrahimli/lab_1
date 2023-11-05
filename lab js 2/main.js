be7cb6e4b4e7ca4ca48cad7ce037606a1ee3364a
Jamil-Ibrahimli~ <ibrahimlicemil@example.com>
2023-11-05 18:15:31 +0400

    lab  JS 2

---
+++ lab js 2/main.js
@@ -0,0 +1,121 @@
+'use strict';
+
+
+
+
+
+
+
+
+
+
+
+// function myFun(reverse){
+
+//  let result = '' ;
+
+// for(let i =reverse.length-1;   i >= 0;   i--){
+
+
+// result += reverse[i]
+
+// }
+
+// return result
+
+// }
+
+// alert(myFun('salam'))
+
+
+
+
+
+
+
+// function myFun(reverse) {
+
+
+//     let result = ""
+//     let leng = result.length;
+
+
+//     // let value = 0
+
+//     for (let i = 0; i < reverse.length; i++) {
+//         if (reverse[i] == "e") { 
+//             // value++
+//         }
+
+
+//     }
+
+
+//     return value
+// }
+
+// console.log(myFun("Never give up"))
+
+
+
+
+
+
+
+// const cars = [
+
+//     {
+//         color: 'red',
+//         type: 'mercedes',
+//         capacity: 5,
+//         price: 120,
+//     },
+//     {
+//         color: 'blue',
+//         type: 'bmw',
+//         capacity: 2,
+//         price: 180
+//     },
+//     {
+//         color: 'blue',
+//         type: 'ford',
+//         capacity: 6,
+//         price: 200
+//     },
+
+//     {
+//         color: 'blue',
+//         type: 'bmw',
+//         capacity: '6',
+//         price: 100
+//     },
+
+// ];
+
+
+
+// let carsSize = cars.map((car) => {
+//     if (car.capacity <= 3) {
+//         return  `small ${car.type}`
+//     }
+//     else if (car.capacity <= 5) { return `medium ${car.type}` }
+
+//     else { return `large ${car.type}` }
+
+
+// })
+
+
+
+
+
+
+// let filt = cars.filter((car)=>car.color='blue').map((car)=>{let discount = car.price - (car.price*30)/100;
+
+// return `new price ${discount} $car model  ${car.type}`
+// })
+
+// console.log(filt)
+
+
+
