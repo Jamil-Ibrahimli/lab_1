c057bbfee9ec59e9e7550f6b5966ea4d4fe61fed
Jamil-Ibrahimli~ <ibrahimlicemil@example.com>
2023-12-20 04:27:49 +0400

    modified -( added to local storage and changes background images)

--- JS lab 13  Weather API/main.js
+++ JS lab 13  Weather API/main.js
@@ -1,109 +1,105 @@
 'use strict'
 
-let weather = document.querySelector('.weather')
+let weatherSide = document.querySelector('.weather');
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
+
     setTimeout(() => {
-        weather.classList.add('active');
+
+        weatherSide.classList.add('active')
 
 
-    }, 1500)
+    }, 2000)
 
 
 })
 
 
+let input = document.querySelector('.weather_container_input_text');
 
-let input = document.querySelector('.weather_container_input_text')
+let icon = document.querySelector('.weather_container_output_icon');
 
-let container = document.querySelector('.weather_container')
+let name = document.querySelector('.weather_container_output_name');
 
+let degree = document.querySelector('.weather_container_output_degree');
 
-let name = document.querySelector('.weather_container_output_name')
-let degree = document.querySelector('.weather_container_output_degree')
-let info = document.querySelector('.weather_container_output_info')
+let info = document.querySelector('.weather_container_output_info');
 
-let icon = document.querySelector('.weather_container_output_icon')
-
-
+let country = document.querySelector('.weather_container_output_country');
 
 
 
 input.addEventListener('keypress', (e) => {
 
-    if (e.keyCode == "13") {
 
-        const key = '8c0f70b1c515df4ededf4dc20f8e5249'
+    if (e.keyCode == '13') {
 
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${key}&units=metric&lang=ru`)
+        const key = '8c0f70b1c515df4ededf4dc20f8e5249';
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${key}&lang=eng&units=metric`)
 
 
             .then((res) => res.json())
-            .then((data) => display(data)
+            .then((data) => display(data))
 
 
-            )
-
-        function display(data) {
-
-            name.innerHTML = `${data.name}`
-            degree.innerHTML = `${Math.round(data.main.temp)}\u00B0C`
-            info.innerHTML = `${data.weather[0].description}  `
-            icon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    }
 
 
+    function display(data) {
 
-            if (data.weather[0].description == 'ясно' ) {
-
-                console.log("HELLO IT IS WORKS")
-
-                document.body.setAttribute('style','background-image:url("'+ 'image/sunny.jpg'  +'");')
-
-            }
-
-            else if (data.weather[0].description == 'пасмурно') { 
-                
-                
-                console.log('JUST HELLO') 
+        name.innerHTML = data.name;
+        degree.innerHTML = `${Math.round(data.main.temp)}\u00B0C`;
+        info.innerHTML = data.weather[0].main;
+        country.innerHTML = data.sys.country;
+        icon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
 
 
-                   document.body.setAttribute('style','background-image:url("'+ 'image/Claud.jpg'  +'");')
-            
-            }
+        if (info.innerHTML == 'Clouds') {
 
-            else if(data.weather[0].description == 'дождь'){
+            document.body.style.backgroundImage = 'url("image/Claud.jpg")';
+
+        } else if (info.innerHTML == 'Clear') {
+
+            document.body.style.backgroundImage = 'url("image/SunnySKy.jpg")';
+
+        } else if (info.innerHTML == 'Snow') {
+
+            document.body.style.backgroundImage = 'url("image/snowing.jpg")';
+
+        } else if (info.innerHTML == 'Rain') {
+
+            document.body.style.backgroundImage = 'url("image/rain.jpg")';
+
+        } else if (info.innerHTML == 'Mist') {
+
+            document.body.style.backgroundImage = 'url("image/mist.jpg")';
+
+        } else if (info.innerHTML == "Thunderstorm") {
 
 
-         document.body.setAttribute('style','background-image:url("'+'image/rain.jpg'+'")')
+            document.body.style.backgroundImage = 'url("image/ThunderStrom.jpg")'
 
-            }
-            else if (data.weather[0].description=='небольшой снег'){
+        }else if (info.innerHTML == 'Drizzle') {
+
+            document.body.style.backgroundImage = 'url("image/rain.jpg")';
         
-
-                document.body.setAttribute('style','background-image:url("'+'image/snowing.jpg'+'")')
-
-
-            }
-
-
-
-
-            localStorage.setItem('name', data.name)
-
-            localStorage.setItem('degree', data.main.temp)
-
-            localStorage.setItem('info', data.weather[0].description)
-
-            localStorage.setItem('icon', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
-
-
-
         }
 
 
+        settingLocal(data)
+    }
 
+
+    function settingLocal(data) {
+
+        localStorage.setItem('name', data.name)
+        localStorage.setItem('degree', data.main.temp)
+        localStorage.setItem('info', data.weather[0].main)
+        localStorage.setItem('country', data.sys.country)
+        localStorage.setItem('icon', data.weather[0].icon)
 
 
 
@@ -111,43 +107,48 @@ input.addEventListener('keypress', (e) => {
 
 
 
-}
-
-
-)
+})
 
 
 
-degree.style.fontWeight = 'bold';
-degree.style.fontSize = '3rem';
-degree.style.color = 'white';
 
-
-name.style.fontWeight = 'bold';
-name.style.fontSize = '2rem';
-name.style.color = 'white';
-
-
-
-info.style.fontWeight = 'bold';
-info.style.fontSize = '1rem';
-info.style.color = 'white';
-
-
-
-name.innerHTML = localStorage.getItem('name');
-
-degree.innerHTML = Math.round(localStorage.getItem('degree')) + '\u00B0C';
-
+name.innerHTML = localStorage.getItem('name')
+degree.innerHTML = `${Math.round(localStorage.getItem('degree'))}\u00B0C`
 info.innerHTML = localStorage.getItem('info')
-
-
-icon.setAttribute("src", localStorage.getItem('icon'))
-
-
+country.innerHTML = localStorage.getItem('country')
+icon.setAttribute('src', `https://openweathermap.org/img/wn/${localStorage.getItem('icon')}.png`)
 
 
 
+if (info.innerHTML == 'Clouds') {
+
+    document.body.style.backgroundImage = 'url("image/Claud.jpg")';
+
+} else if (info.innerHTML == 'Clear') {
+
+    document.body.style.backgroundImage = 'url("image/SunnySKy.jpg")';
+
+} else if (info.innerHTML == 'Snow') {
+
+    document.body.style.backgroundImage = 'url("image/snowing.jpg")';
+
+} else if (info.innerHTML == 'Rain') {
+
+    document.body.style.backgroundImage = 'url("image/rain.jpg")';
+
+} else if (info.innerHTML == 'Drizzle') {
+
+    document.body.style.backgroundImage = 'url("image/rain.jpg")';
+
+}
+else if (info.innerHTML == 'Mist') {
+
+    document.body.style.backgroundImage = 'url("image/mist.jpg")';
+
+} else if (info.innerHTML == "Thunderstorm") {
 
 
+    document.body.style.backgroundImage = 'url("image/ThunderStrom.jpg")'
+
+}
 
